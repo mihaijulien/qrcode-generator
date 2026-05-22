2c69310d3a8b2dc41b07fb36114e0817d331be2a
mihaijulien <mihaijulien@gmail.com>
2026-05-22 12:25:47 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-rose-400 to-indigo-400">
+        <h1 className="text-6xl font-bold text-white">
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
